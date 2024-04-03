3a22ff2730dd5f2017197e87d1e379e029b03f87
Jason Zheng <jasonz4200@gmail.com>
2024-04-03 04:38:02 -0400

    Configure Tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,11 @@
 /** @type {import("tailwindcss").Config} */
 const config = {
+  prefix: "tw-",
+
+  corePlugins: {
+    preflight: false,
+  },
+
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
