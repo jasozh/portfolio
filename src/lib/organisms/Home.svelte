8cbaaa86553c7cbb2f09c910295ee422e57956af
Jason Zheng <jasonz4200@gmail.com>
2024-04-03 17:21:57 -0400

    Move home css

--- src/lib/organisms/Home.svelte
+++ src/lib/organisms/Home.svelte
@@ -36,3 +36,113 @@
     >
   </div>
 </div>
+
+<style>
+  /* Ensures that the main button group on Home is responsive */
+  @media screen and (max-width: 991px) {
+    .button-group {
+      margin-left: 100px;
+      margin-right: 100px;
+    }
+  }
+
+  @media screen and (min-width: 992px) {
+    .button-group {
+      min-width: 700px;
+    }
+  }
+
+  /* Make buttons slightly transparent until hover */
+  .transparent {
+    opacity: 0.75;
+  }
+
+  .transparent:hover {
+    opacity: 1;
+  }
+
+  /* Profile picture styling and animations */
+  img.profile {
+    border-radius: 50%;
+    max-height: 300px;
+    max-width: 50%;
+    cursor: pointer;
+    animation-name: profile-animation;
+    animation-duration: 1s;
+  }
+
+  img.profile:active {
+    transform: scale(0.8, 0.8) rotate(10deg);
+  }
+
+  /* Title and subtitle text animations */
+  @keyframes profile-animation {
+    0% {
+      transform: scale(0.7, 0.7);
+      opacity: 0.5;
+    }
+
+    100% {
+      opacity: 1;
+    }
+  }
+
+  .title-text {
+    animation-name: title-animation;
+    animation-duration: 1s;
+  }
+
+  @keyframes title-animation {
+    0% {
+      opacity: 0;
+    }
+
+    25% {
+      opacity: 0;
+    }
+
+    100% {
+      opacity: 1;
+    }
+  }
+
+  .subtitle-text {
+    animation-name: subtitle-animation;
+    animation-duration: 1.25s;
+  }
+
+  @keyframes subtitle-animation {
+    0% {
+      opacity: 0;
+    }
+
+    50% {
+      opacity: 0;
+    }
+
+    100% {
+      opacity: 1;
+    }
+  }
+
+  /* Button group animations */
+  .button-group {
+    animation-name: button-animation;
+    animation-duration: 2s;
+  }
+
+  @keyframes button-animation {
+    0% {
+      opacity: 0;
+      transform: translate(0px, 100px);
+    }
+
+    50% {
+      opacity: 0;
+    }
+
+    100% {
+      opacity: 1;
+    }
+  }
+</style>
--- src/lib/templates/HomeTemplate.svelte
+++ src/lib/templates/HomeTemplate.svelte
@@ -14,3 +14,26 @@
     <div class="py-5"></div>
   </div>
 </article>
+
+<style>
+  /* Sets background image and pseudoelement for transparency */
+  article#home {
+    background-image: url("../img/background.png");
+  }
+
+  article#home::before {
+    content: "";
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
+    background-color: rgba(255, 255, 255, 0.25);
+  }
+
+  /* Ensures actual content is not affected by the transparent pseudolayer */
+  .home-content {
+    position: relative;
+    text-align: center;
+  }
+</style>
